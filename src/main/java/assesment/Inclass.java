1f66750a3eac9a653f7ab83c269a18e85d9893dd
Nusrat Jahan <jahan8791@gmail.com>
2023-03-24 15:31:00 -0400

    Initial commit

---
+++ src/main/java/assesment/Inclass.java
@@ -0,0 +1,70 @@
+package assesment;
+
+import javax.print.attribute.standard.PrinterName;
+
+public class Inclass {
+                static void printName(){
+                        System.out.println("Nusrat Jahan");
+                }
+
+//                static int greetUser(String userName ){
+//                        System.out.println( "Welcome to my website" + userName);
+//                        //greetUser("by Nusrat");
+//                     //  return greetUser("by Nusrat");
+//                }
+
+                public static void buyCoffee(int dollars){
+                     int c = 1;
+                         System.out.println(c);
+                     }
+
+            public static int myHobbies(int hobbies){
+              String[] hobbies = ("") ;
+
+            }
+
+        public static void main(String[] arg){
+                   // question -1
+            printName();
+
+                    // Question - 2
+            // System.out.println(greetUser ("by Nusrat"));
+//                      greetUser("by Nusrat");
+
+            return System.out.println(buyCoffee(dollar 1,);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
